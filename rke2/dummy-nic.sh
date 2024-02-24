a1e1c3dfa8104e166cdc1ff8609ad91e9457c075
Jacob May <jacob.may@defenseunicorns.com>
2024-02-24 01:04:23 +0000

    comment formatting

--- rke2/dummy-nic.sh
+++ rke2/dummy-nic.sh
@@ -2,6 +2,7 @@
 
 # This script creates a dummy interface, default route, and a nameserver entry to support running RKE2 on baremetal without being connected to any network.
 # If RKE2 is installed while connected, make sure to update the IPs to match the network that RKE2 was installed with otherwise etcd will not start up. 
+# 
 # The server can also be reconnected to a network as long as it is assigned the same IP that RKE2 was installed with. Simply power the machine off, reconnect
 # networking cables, and power the machine back on. On startup, the dummy interface should be gone and the default interface should be configured instead, and
 # RKE2 should be running or in the process of starting back up
